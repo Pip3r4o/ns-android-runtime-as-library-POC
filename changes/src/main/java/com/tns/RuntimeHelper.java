59690e7dd959923ad54acb9ec9012e484e7411fc
Peter Kanev <pip3r4o@gmail.com>
2016-05-16 17:23:38 +0300

    add specific changed files to make the thing work

---
+++ changes/src/main/java/com/tns/RuntimeHelper.java
@@ -0,0 +1,135 @@
+package com.tns;
+
+import java.io.File;
+
+import android.app.Application;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import java.io.IOException;
+
+public class RuntimeHelper
+{
+	private final Application app;
+	private Runtime runtime;
+	
+	public RuntimeHelper(Application app)
+	{
+		this.app = app;
+	}
+	
+	// hasErrorIntent tells you if there was an event (with an uncaught exception) raised from ErrorReport
+	public boolean hasErrorIntent()
+	{
+		boolean hasErrorIntent = false;
+		
+		try
+		{
+			 //empty file just to check if there was a raised uncaught error by ErrorReport
+			File errFile = new File(app.getFilesDir(), ErrorReport.ERROR_FILE_NAME);
+			
+			if (errFile.exists())
+			{
+				errFile.delete();
+				hasErrorIntent = true;
+			}
+		}
+		catch (Exception e)
+		{
+			Log.d(logTag, e.getMessage());
+		}
+		
+		return hasErrorIntent;
+	}
+	
+	public void initRuntime()
+	{
+
+		Log.d("~~~1~~~", "Before System.loadLibrary NativeScript");
+
+		System.loadLibrary("NativeScript");
+		
+		Logger logger = new LogcatLogger(true, app);
+		
+		boolean showErrorIntent = hasErrorIntent();
+		if (!showErrorIntent)
+		{
+			NativeScriptUncaughtExceptionHandler exHandler = new NativeScriptUncaughtExceptionHandler(logger, app);
+
+			Thread.setDefaultUncaughtExceptionHandler(exHandler);
+			
+			Async.Http.setApplicationContext(this.app);
+			
+			ExtractPolicy extractPolicy = new DefaultExtractPolicy(logger);
+
+			new AssetExtractor(null, logger).extractAssets(app, extractPolicy);
+
+			String appName = app.getPackageName();
+			File rootDir = new File(app.getApplicationInfo().dataDir);
+			File appDir = app.getFilesDir();
+			
+			try
+			{
+				appDir = appDir.getCanonicalFile();
+			}
+			catch (IOException e1)
+			{
+			}
+
+			ClassLoader classLoader = app.getClassLoader();
+			File dexDir = new File(rootDir, "code_cache/secondary-dexes");
+			String dexThumb = null;
+			try
+			{
+				dexThumb = Util.getDexThumb(app);
+			}
+			catch (NameNotFoundException e)
+			{
+				if (logger.isEnabled()) logger.write("Error while getting current proxy thumb");
+				e.printStackTrace();
+			}
+			ThreadScheduler workThreadScheduler = new WorkThreadScheduler(new Handler(Looper.getMainLooper()));
+			Configuration config = new Configuration(this.app, workThreadScheduler, logger, appName, null, rootDir, appDir, classLoader, dexDir, dexThumb);
+
+			Log.d("~~~1~~~", "Before new Runtime");
+			this.runtime = new Runtime(config);
+
+			Log.d("~~~1~~~", "runtime after new Runtime() -> " + this.runtime != null ? "NOT NULL" : "NULL");
+
+			exHandler.setRuntime(this.runtime);
+			
+			if (NativeScriptSyncService.isSyncEnabled(this.app))
+			{
+				NativeScriptSyncService syncService = new NativeScriptSyncService(this.runtime, logger, this.app);
+
+				syncService.sync();
+				syncService.startServer();
+			}
+			else
+			{
+				if (logger.isEnabled())
+				{
+					logger.write("NativeScript LiveSync is not enabled.");
+				}
+			}
+			
+			this.runtime.init();
+
+			Log.d("~~~1~~~", "After runtime.init() -> " + this.runtime != null ? "NOT NULL" : "NULL");
+
+			// runtime.runScript(new File(appDir, "internal/ts_helpers.js"));
+			// Runtime.initInstance(this.app);
+			// runtime.run();
+
+			Log.d("~~~1~~~", "runtime at the end of initRuntime RuntimeHelper is " + this.runtime != null ? "NOT NULL" : "NULL");
+		}
+	}
+
+	public Object runScript(File jsFile) {
+		Log.d("~~~2~~~", "runtime inside RuntimeHelper is " + this.runtime != null ? "NOT NULL" : "NULL");
+		return this.runtime.runScript(jsFile);
+	}
+	
+	private final String logTag = "MyApp";
+}
