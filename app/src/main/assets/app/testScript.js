80b6b9d3ceb54156cb34f77cba4cc5e28342c4cc
Peter Kanev <pip3r4o@gmail.com>
2016-05-16 11:48:00 +0300

    Initial commit

---
+++ app/src/main/assets/app/testScript.js
@@ -0,0 +1,8 @@
+(function() {
+ __log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ WOOOOOOOOOOOOOOOOOOHOOOOOOOOOOOOOOOOO ~~~~~~~~~~~~~~~~~~~")
+ // console is not recognized - console.log("Moooooooo")
+
+ var result = require("simplemodule").myLog("pesho");
+
+ return result;
+})()
---
+++ app/src/main/assets/app/tns_modules/simplemodule/index.js
@@ -0,0 +1,14 @@
+var summer = require('../simplesubmodule')
+
+exports.myLog = function(s)
+{
+	if (arguments.length == 1)
+	{
+//		__log("1+1=" + summer.add(1, 1))
+//
+//		__log("global is working" + global)
+	}
+//	__log(s.toString());
+
+	return s;
+}
